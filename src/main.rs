56a4d090f7802c299faa8b6815f74d7ddc88267f
Laith Bahodi <laithbahodi@gmail.com>
2021-12-02 00:41:25 -0500

    day2 solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod day1;
+mod day2;
 
-use crate::day1::{solve_day1p1, solve_day1p2};
-
+//use crate::day1::{solve_day1p1, solve_day1p2};
+use crate::day2::{solve_day2p1, solve_day2p2};
 
 fn main() {
-    dbg!(solve_day1p1());
-    dbg!(solve_day1p2());
+    // dbg!(solve_day1p1());
+    // dbg!(solve_day1p2());
+    dbg!(solve_day2p1());
+    dbg!(solve_day2p2());
+
 }
