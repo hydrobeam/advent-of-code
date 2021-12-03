dc9530566ad9e663e80ed3c142246a2ce86063ce
Laith Bahodi <laithbahodi@gmail.com>
2021-12-03 06:06:17 -0500

    add day3 solutions

---
+++ src/day3.rs
@@ -0,0 +1,118 @@
+use std::fs;
+use std::str::FromStr;
+
+fn solve_day3p1() {
+    let filename = "inputs/day3input.txt";
+    let contents = fs::read_to_string(filename).expect("they makin me do this");
+    let vecsquad = contents.lines();
+
+    let mut i: u16 = 0;  
+    let mut hs: Vec<u16> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for bit in vecsquad {
+        i += 1;
+        let lst = bit.split_terminator("").into_iter().skip(1);
+
+        for (index, item) in lst.enumerate() {
+            hs[index] += u16::from_str(item).unwrap();
+        }
+    }
+
+
+    let ho = hs
+        .iter()
+        .map(|&x| if x < i / 2 { 0 } else { 1 }).fold(0, |mut total, x| {
+            total *= 2;
+            total + x
+        });
+
+    let hm = hs
+        .iter()
+        .map(|&x| if x < i / 2 { 1 } else { 0 }).fold(0, |mut total, x| {
+            total *= 2;
+            total + x
+        });
+
+}
+
+fn solve_day3p2() {
+    let filename = "inputs/day3input.txt";
+    let contents = fs::read_to_string(filename).expect("they makin me do this");
+    let vecsquad = contents.lines();
+    
+    let mut oxy_iter = vecsquad
+        .clone()
+        .filter(|&x| u8::from_str(x.split_terminator("").nth(1).unwrap()).unwrap() == 0)
+        .collect::<Vec<&str>>()
+        .into_iter();
+
+    for pos in 2..12 {
+        let craw = oxyen_rating(do_stuff(oxy_iter.clone(), pos), oxy_iter.clone().count() as u16);
+        oxy_iter = oxy_iter
+            .filter(|x| {
+                u8::from_str(x.split_terminator("").nth(pos).unwrap()).unwrap() == craw
+            })
+            .collect::<Vec<&str>>()
+            .into_iter();
+        if oxy_iter.len() == 1 {
+            println!("break");
+            break;
+        }
+    }
+
+    println!("{}", oxy_iter.next().unwrap());
+
+    let mut c02_iter = vecsquad
+        .clone()
+        .filter(|&x| u8::from_str(x.split_terminator("").nth(1).unwrap()).unwrap() == 1)
+        .collect::<Vec<&str>>()
+        .into_iter();
+
+    for pos in 2..12 {
+        let craw = c02_rating(do_stuff(c02_iter.clone(), pos), c02_iter.clone().count() as u16);
+        c02_iter = c02_iter
+            .filter(|x| u8::from_str(x.split_terminator("").nth(pos).unwrap()).unwrap() == craw)
+            .collect::<Vec<&str>>()
+            .into_iter();
+
+        if c02_iter.len() == 1 {
+            println!("break");
+            break;
+        }
+    }
+    println!("{}", c02_iter.next().unwrap());
+}
+
+fn do_stuff<'a, T>(vec: T, pos: usize) -> u16
+where
+    T: Iterator<Item = &'a str>,
+{
+    let mut ind: u16 = 0;
+    for bit in vec {
+        ind +=  u16::from_str(bit.split_terminator("").into_iter().nth(pos).unwrap()).unwrap();
+    }
+    ind
+}
+
+fn oxyen_rating(num: u16, comp: u16) -> u8 {
+    if num == comp / 2 {
+        // always rounds down  which could cause bugs
+        1
+    } else if num > comp / 2 {
+        1
+    } else {
+        0
+    }
+}
+
+fn c02_rating(num: u16, comp: u16) -> u8 {
+    // dbg!(comp);
+    if num == comp / 2 {
+        // always rounds down which could cause  bugs
+        0
+    } else if num > comp / 2 {
+        0
+    } else {
+        1
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod day1;
 mod day2;
+mod day3;
+
+use std::fs;
+use std::str::FromStr;
 
 //use crate::day1::{solve_day1p1, solve_day1p2};
-use crate::day2::{solve_day2p1, solve_day2p2};
+// use crate::day2::{solve_day2p1, solve_day2p2};
 
 fn main() {
-    // dbg!(solve_day1p1());
-    // dbg!(solve_day1p2());
-    dbg!(solve_day2p1());
-    dbg!(solve_day2p2());
 
 }
