aab3a4a66bd9dc814e5e8211ce87e53838af5af5
Laith Bahodi <laithbahodi@gmail.com>
2021-12-06 04:40:27 -0500

    Add day6 solutions

---
+++ src/day6.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+pub fn solve_day6() {
+    let contents = include_str!("../inputs/day6input.txt");
+    let fish_timers = contents
+        .split_terminator(',')
+        .map(|x| x.parse::<i64>().expect(x))
+        .collect::<Vec<i64>>();
+    let days = 0;
+
+    let mut hs: HashMap<i64, i64> = HashMap::new();
+
+    // janky ass solution but hey
+    
+    // brute force the first bit up to a certain point where it's fast enough
+    for elem in crack(fish_timers, days, 130) {
+        *hs.entry(elem).or_insert(0) += 1;
+    }
+
+    let mut sum: i64 = 0;
+    // the fishies grow predictably, so just simulate one growing
+    // for the remaining days
+    // then multiply however many it produces by how many times it appears
+    // in the original days.
+
+    for swag in hs.keys() {
+        let ve = vec![*swag];
+        sum += crack(ve, 0, 126).len() as i64 * hs.get(swag).unwrap();
+    }
+
+    dbg!(hs);
+    dbg!(sum);
+}
+
+fn crack(mut vec: Vec<i64>, mut curr_days: i64, lim_days: i64) -> Vec<i64> {
+    // brute forces and runs the simulation of fishy reproduction
+
+    while curr_days < lim_days {
+        let mut cnt: u32 = 0;
+        for item in &mut vec {
+            *item -= 1;
+            if item.is_negative() {
+                *item = 6;
+                cnt += 1;
+            }
+        }
+        vec.append(&mut vec![8; cnt as usize]);
+        curr_days += 1;
+    }
+    vec
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day2_2020;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 // use std::cmp::{max, min};
 // use day1_2020::solve2020_day1p1;
@@ -15,8 +16,8 @@ mod day5;
 // use crate::day2::{solve_day2p1, solve_day2p2};
 // use crate::day3::{solve_day3p1, solve_day3p2};
 // use crate::day4::{solve_day4p1, solve_day4p2};
-use crate::day5::{solve_day5p1, solve_day5p2};
+// use crate::day5::{solve_day5p1, solve_day5p2};
+use crate::day6::{solve_day6};
 fn main() {
-    solve_day5p1();
-    solve_day5p2();
+    solve_day6();
 }
