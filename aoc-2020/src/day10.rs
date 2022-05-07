332d446de6cdce10f60e64fb2a48d277e7bcfd15
Laith Bahodi <laithbahodi@gmail.com>
2022-05-07 07:03:29 -0400

    Some performance improvements to day10 2020

--- aoc-2020/src/day10.rs
+++ aoc-2020/src/day10.rs
@@ -7,49 +7,44 @@ pub fn solve_day10() {
 
     content.push(0);
     content.sort_unstable();
-    content.push(content[content.len() -1] + 3);
+    content.push(content[content.len() - 1] + 3);
 
+    // PART 1 STUFF
+    // tracks how many ones and threes have been encountered in total
     let mut three_count = 0;
     let mut one_count = 0;
-    let mut dist_vec: Vec<u64> = Vec::with_capacity(content.len());
+
+    // PART 2 STUFF
+    // tracks how many consecutive jumps of 1 we've run into
+    let mut running_sum = 0;
+
+    let mut solutions: u64 = 1;
 
     for group in content.windows(2) {
         match group[1] - group[0] {
             3 => {
                 three_count += 1;
-                dist_vec.push(3)
+
+                if running_sum > 0 {
+                    // find the sum of the numbers from 1 to running sum:
+                    // (n)(n+1) / 2
+                    // then subtract (n - 1)
+                    //
+                    // as for why this works, i have no idea, found through trial and error.
+                    solutions *= (running_sum * (running_sum + 1) / 2) - (running_sum - 1);
+                    running_sum = 0;
+                }
             }
             1 => {
                 one_count += 1;
-                dist_vec.push(1)
+
+                running_sum += 1;
             }
             _ => {}
         }
     }
 
     println!("The solution to part 1: {}", three_count * one_count);
-
-    let mut solutions: u64 = 1;
-
-    // tracks how many consecutuive ones we've run into within dist_vec
-    let mut running_sum = 0;
-
-    for val in dist_vec {
-        if val == 1 {
-            running_sum += 1;
-        } else {
-            if running_sum > 0 {
-                // find the sum of the numbers from 1 to running sum:
-                // (n)(n+1) / 2
-                // then subtract (n - 1)
-                //
-                // as for why this works, i have no idea, found through trial and error. 
-                solutions *= (running_sum * (running_sum + 1) / 2) - (running_sum - 1);
-                running_sum = 0;
-            }
-        }
-    }
-
     println!("The solution to part 2 is: {}", solutions);
 
     // worked throgh in ../day10_thinking/test.txt
@@ -66,7 +61,7 @@ pub fn solve_day10() {
 }
 
 // thinking process
-// 
+//
 //fn rec_func(num: u64, acc: u64) -> u64 {
 //    if num == 1 {
 //        acc
