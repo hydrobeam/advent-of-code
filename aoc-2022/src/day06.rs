1f3e0c4a0dcc6f4126001f9d63d00c10ca9c0a26
Laith Bahodi <laithbahodi@gmail.com>
2022-12-06 00:48:07 -0500

    2022: cleanup day 6 solution
    use hashset and condense into one loop

--- aoc-2022/src/day06.rs
+++ aoc-2022/src/day06.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn solve() {
     let content = include_str!("../inputs/day06.txt");
 
@@ -5,28 +7,42 @@ pub fn solve() {
 
     let marker_count_1 = 4;
     let marker_count_2 = 14;
+    // det_1 determines whether 1 has been found
+    // only det_1 is needed because 4 non duplicate elements must occur before we get 14
+    let mut det_1 = false;
 
-    for count in 0..content_vec.len() {
-        if count > marker_count_1 - 1 {
-            let mut temp_vec = content_vec[(count - (marker_count_1 - 1))..=count].to_owned();
-            temp_vec.sort_unstable();
-            temp_vec.dedup();
-            if temp_vec.len() == marker_count_1 {
-                dbg!(count + 1);
-                break;
+
+    let mut sol_1: usize = 0;
+    let mut sol_2: usize = 0;
+
+    for count in marker_count_1..content_vec.len() {
+        if !det_1 {
+            if detect_dup(
+                &content_vec[(count - (marker_count_1 - 1))..=count],
+                marker_count_1,
+            ) {
+                det_1 = true;
+                sol_1 = count + 1;
             }
         }
-    }
 
-    for count in 0..content_vec.len() {
         if count > marker_count_2 - 1 {
-            let mut temp_vec = content_vec[(count - (marker_count_2 - 1))..=count].to_owned();
-            temp_vec.sort_unstable();
-            temp_vec.dedup();
-            if temp_vec.len() == marker_count_2 {
-                dbg!(count + 1);
+            if detect_dup(
+                &content_vec[(count - (marker_count_2 - 1))..=count],
+                marker_count_2,
+            ) {
+                sol_2 = count + 1;
                 break;
             }
         }
     }
+
+    println!("Part 1: {}", sol_1);
+    println!("Part 2: {}", sol_2);
+}
+
+#[inline]
+fn detect_dup(list: &[char], past: usize) -> bool {
+    let temp_set: HashSet<char> = list.iter().copied().collect();
+    temp_set.len() == past
 }
