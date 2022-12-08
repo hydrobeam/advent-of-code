f7131ce4d16b9d74b3b6b64b0a5228e58820fd87
Laith Bahodi <laithbahodi@gmail.com>
2022-12-08 02:58:54 -0500

    2022: cleanup day 7

--- aoc-2022/src/day07.rs
+++ aoc-2022/src/day07.rs
@@ -1,8 +1,9 @@
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
-const TOT_DISK_SPACE: isize = 70000000;
-const REQUIRED_UPDATE_SIZE: isize = 30000000;
+const TOT_DISK_SPACE: usize = 70000000;
+const REQUIRED_UPDATE_SIZE: usize = 30000000;
+const FILE_SIZE_LIMIT: usize = 100000;
 
 pub fn solve() {
     let content: Vec<Vec<char>> = include_str!("../inputs/day07.txt")
@@ -10,23 +11,17 @@ pub fn solve() {
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect();
 
-    let base = Rc::new(Dir {
-        name: "/".to_string(),
-        parent: RefCell::new(Weak::new()),
-        child_dirs: RefCell::new(Vec::new()),
-        direct_files: RefCell::new(Vec::new()),
-    });
+    let base = Rc::new(Dir::new("/".to_string(), &RefCell::default()));
 
     let curr_dir: RefCell<Weak<Dir>> = RefCell::new(Rc::downgrade(&base));
 
     for line in &content {
         if line[0] == '$' {
             if line[2] == 'l' {
+                // $ ls
+                // do nothing, just handle its input when it comes
             }
-            // skip
-
-            // now:
-            // must be cd, in the form of:
+            // now the command must be cd, in the form of:
             // $ cd name...
             // 012345
             // first index of name is 5
@@ -44,82 +39,76 @@ pub fn solve() {
                 curr_dir.replace(Rc::downgrade(&base));
             } else {
                 // $ cd name..
-                // we move into a dir
+                // move into a dir, assume it already has been displayed by ls,
+                // i.e. is trackd in the fs
                 let name: String = line[5..].iter().collect();
-                let nu_dir = Rc::new(Dir {
-                    name: name.clone(),
-                    parent: RefCell::clone(&curr_dir),
-                    child_dirs: RefCell::new(Vec::new()),
-                    direct_files: RefCell::new(Vec::new()),
-                });
-                // we assume dups don't happen:
-                {
-                    Weak::upgrade(&*curr_dir.borrow())
-                        .unwrap()
-                        .child_dirs
-                        .borrow_mut()
-                        .push(Rc::clone(&nu_dir));
-                }
+                let temp_dir: Rc<Dir> = (*curr_dir.borrow()).upgrade().unwrap();
+                let nu_vec: &Vec<Rc<Dir>> = &*temp_dir.child_dirs.borrow();
+                let found_dir: &Rc<Dir> = nu_vec.iter().find(|x| x.name == name).unwrap();
 
-                curr_dir.replace(Rc::downgrade(&nu_dir));
+                curr_dir.replace(Rc::downgrade(found_dir));
             }
         } else if line[0] == 'd' {
             // d for dir
+            // adds the item into our fs, we assume duplicates don't happen,
+            // i.e. ls is never called twice
+            //
+            // format:
             // dir 12..
             // 01234
             // name starts at 4
             let name = line[4..].iter().collect();
 
-            let nu_dir = Rc::new(Dir {
-                name,
-                parent: RefCell::clone(&curr_dir),
-                child_dirs: RefCell::new(Vec::new()),
-                direct_files: RefCell::new(Vec::new()),
-            });
-            Weak::upgrade(&*curr_dir.borrow())
+            let nu_dir = Rc::new(Dir::new(name, &curr_dir));
+            // Breakdown of how adding works:
+
+            // curr_dir is a RefCell<Weak<Dir>> (the parent Type)
+            curr_dir
+                .borrow() // we get a Ref<Weak<Dir>>, which we can operate on
+                .upgrade()
+                // upgrade the Weak ref into an Rc, so we can operate on it
+                //  from the docs: it delays dropping of the inner value if successful
+                //  basically, while we're using it, don't destroy it
                 .unwrap()
+                // if the thing it's pointing is destroyed, then we get None
+                // however, with our DS, this doesn't happen. so it always returns the val.
                 .child_dirs
-                .borrow_mut()
+                .borrow_mut() // we need to borrow_mut to mutate the child_dirs
                 .push(nu_dir);
         } else {
             // file stuff
             // the first thing is a number which indicates file size, so just go for it.
             // 57426 gszshjwr.lrs
-            // TODO make this less bad
-            //
-            let mut ind = 0;
-            while line[ind] != ' ' {
-                ind += 1;
-            }
+
+            // find the index of the space and split at it.
+            let ind = line.iter().position(|chr| *chr == ' ').unwrap();
             let (size, name) = line.split_at(ind);
 
-            Weak::upgrade(&*curr_dir.borrow_mut())
+            let name = name[1..].iter().collect();
+            let size = size.iter().collect::<String>().parse::<usize>().unwrap();
+            curr_dir
+                .borrow()
+                .upgrade()
                 .unwrap()
                 .direct_files
                 .borrow_mut()
-                .push(Rc::new(File {
-                    // split_at includes the splitting character for the right side,
-                    // so adjust by 1
-                    name: name[1..].iter().collect(),
-                    size: size.iter().collect::<String>().parse::<isize>().unwrap(),
-                }))
+                .push(Rc::new(File { name, size }))
         }
-
-        // dbg!(&base);
     }
-    let mut var = 0;
-    let curr_size = base.tot_size(&mut var);
-    let mut del_target = curr_size;
+    let mut size_of_files_over_limit = 0;
+    let curr_size = base.tot_size(&mut size_of_files_over_limit);
+    let mut del_target_size = curr_size;
 
-    println!("Part 1: {var}");
-    base.target_deletion(curr_size, &mut del_target);
-    println!("Part 2: {del_target}");
+    base.target_deletion(curr_size, &mut del_target_size);
+    println!("Part 1: {size_of_files_over_limit}");
+    println!("Part 2: {del_target_size}");
 }
 
 // We need these to be RefCell to enable interior mutability.
 // Why? Well when we ascend the fs with `cd ..`, we will be mutating
 // the "curr_dir" tracker. So it needs access to the internals.
 // I.e. it allows us to navigate the fs.
+// we can't borrow data out of an RC NOTE
 #[derive(Debug, Clone)]
 struct Dir {
     name: String,
@@ -128,45 +117,56 @@ struct Dir {
     direct_files: RefCell<Vec<Rc<File>>>,
 }
 
-// to prevent dups, why not use a map..?
-// because we can't. the parent pointer isn't hashable
-// we could try working off names
-// but.. then we'd need full paths. (i think)
-// also, dup detection isn't necessary because the input doesn't try to cd
-// into directories that have not yet been revealed ls
+/// to prevent dups, why not use a map..?
+/// because we can't. the parent pointer isn't hashable
+/// we could try working off names
+/// but.. then we'd need full paths. (i think)
+/// also, dup detection isn't necessary because the input doesn't try to cd
+/// into directories that have not yet been revealed ls
 
 impl Dir {
-    fn tot_size(&self, track_sum: &mut isize) -> isize {
-        let swag = &*self.direct_files.borrow();
-        // dbg!(&swag);
-        let swag_2 = &*self.child_dirs.borrow();
-        let sum = swag.iter().fold(0, |acc, file| file.size + acc)
-            + swag_2
-                .iter()
-                // .inspect(|x| println!("mapping {}", x.name))
-                .map(|child| child.tot_size(track_sum))
-                .sum::<isize>();
-        // dbg!(sum)
+    fn new(name: String, parent: &RefCell<Weak<Dir>>) -> Self {
+        Dir {
+            name,
+            parent: RefCell::clone(parent),
+            child_dirs: RefCell::new(Vec::new()),
+            direct_files: RefCell::new(Vec::new()),
+        }
+    }
 
-        // dbg!(&self.name, sum);
-        if sum <= 100000 {
+    /// Actually solving the problem:
+    /// these two functions do the same thing (see size of dirs) and both do it
+    /// in a bad + weird way: passing around a pointer that gets mutated
+    /// TODO maybe store fize in the directory itself, so that accessing is O(1)?
+
+    /// Find the sum of all directories under the FILE_SIZE_LIMIT
+    fn tot_size(&self, track_sum: &mut usize) -> usize {
+        let files = &*self.direct_files.borrow();
+        let dirs = &*self.child_dirs.borrow();
+        let sum = files.iter().fold(0, |acc, file| file.size + acc)
+            + dirs
+                .iter()
+                .map(|child: &Rc<Dir>| child.tot_size(track_sum))
+                .sum::<usize>();
+        if sum <= FILE_SIZE_LIMIT {
             *track_sum += sum;
-            // println!("{sum}");
         }
         sum
     }
 
-    fn target_deletion(&self, tot_size: isize, del_target_size: &mut isize) -> isize {
-        let swag = &*self.direct_files.borrow();
-        // dbg!(&swag);
-        let swag_2 = &*self.child_dirs.borrow();
-        let sum = swag.iter().fold(0, |acc, file| file.size + acc)
-            + swag_2
+    /// we need to have REQUIRE_UPDATE_SIZE left available to update,
+    /// so we want to find the smallest
+    /// directory to delete that lets us do this
+    fn target_deletion(&self, tot_size: usize, del_target_size: &mut usize) -> usize {
+        let files = &*self.direct_files.borrow();
+        let dirs = &*self.child_dirs.borrow();
+        let sum = files.iter().fold(0, |acc, file| file.size + acc)
+            + dirs
                 .iter()
-                // .inspect(|x| println!("mapping {}", x.name))
                 .map(|child| child.target_deletion(tot_size, del_target_size))
-                .sum::<isize>();
-        if TOT_DISK_SPACE - (tot_size - sum) >= REQUIRED_UPDATE_SIZE {
+                .sum::<usize>();
+
+        if TOT_DISK_SPACE - REQUIRED_UPDATE_SIZE >= (tot_size - sum) {
             if sum <= *del_target_size {
                 *del_target_size = sum;
             }
@@ -178,9 +178,5 @@ impl Dir {
 #[derive(Debug, Clone)]
 struct File {
     name: String,
-    size: isize,
+    size: usize,
 }
-
-// trait Command {
-//     fn run();
-// }
