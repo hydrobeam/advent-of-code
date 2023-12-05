e8c9678dd326cc6f69cbc283ad582d79e07d73f4
Laith <laithbahodi@gmail.com>
2023-12-05 11:40:30 -0500

    2023: day2

--- aoc-2023/src/days/day02.rs
+++ aoc-2023/src/days/day02.rs
@@ -3,12 +3,102 @@ use crate::solutions::{AocSol, Solution};
 
 pub struct Day02;
 
+enum Colour {
+    Red(usize),
+    Green(usize),
+    Blue(usize),
+}
+
+impl From<(u8, usize)> for Colour {
+    fn from(value: (u8, usize)) -> Self {
+        match value {
+            (b'r', s) => Colour::Red(s),
+            (b'g', s) => Colour::Green(s),
+            (b'b', s) => Colour::Blue(s),
+            _ => unreachable!(),
+        }
+    }
+}
+
 impl Solution for Day02 {
-    fn solve_p1(_input: &str) -> AocSol {
-        unimplemented!("This day has not been implemented yet.")
+    fn solve_p1(input: &str) -> AocSol {
+        const RED_LIMIT: usize = 12;
+        const GREEN_LIMIT: usize = 13;
+        const BLUE_LIMIT: usize = 14;
+
+        input
+            .lines()
+            .map(|line| {
+                let ind = line.find(':').unwrap();
+                let id = line[5..ind].parse::<usize>().unwrap();
+
+                let mut rolls = line[ind + 1..].split(';').map(|roll| {
+                    let mut r = 0;
+                    let mut g = 0;
+                    let mut b = 0;
+                    roll.split(',')
+                        .map(|item| {
+                            let mut a = item.trim().split(' ');
+                            let num = a.next().unwrap().parse::<usize>().unwrap();
+                            let colour = a.next().unwrap();
+                            Colour::from((colour.as_bytes()[0], num))
+                        })
+                        .for_each(|col| match col {
+                            Colour::Red(s) => r = r.max(s),
+                            Colour::Green(s) => g = g.max(s),
+                            Colour::Blue(s) => b = b.max(s),
+                        });
+                    r > RED_LIMIT || b > BLUE_LIMIT || g > GREEN_LIMIT
+                });
+
+                if rolls.any(|a| a) {
+                    0
+                } else {
+                    id
+                }
+            })
+            .sum::<usize>()
+            .into()
     }
 
-    fn solve_p2(_input: &str) -> AocSol {
-        unimplemented!("This day has not been implemented yet.")
+    fn solve_p2(input: &str) -> AocSol {
+        input
+            .lines()
+            .map(|line| {
+                let ind = line.find(':').unwrap();
+                let _id = line[5..ind].parse::<usize>().unwrap();
+
+                line[ind + 1..]
+                    .split(';')
+                    .map(|roll| {
+                        let mut r = 0;
+                        let mut g = 0;
+                        let mut b = 0;
+                        roll.split(',')
+                            .map(|item| {
+                                let mut a = item.trim().split(' ');
+                                let num = a.next().unwrap().parse::<usize>().unwrap();
+                                let colour = a.next().unwrap();
+                                Colour::from((colour.as_bytes()[0], num))
+                            })
+                            .for_each(|col| match col {
+                                Colour::Red(s) => r = r.max(s),
+                                Colour::Green(s) => g = g.max(s),
+                                Colour::Blue(s) => b = b.max(s),
+                            });
+                        [r, g, b]
+                    })
+                    .fold([0, 0, 0], |curr_max, x| {
+                        [
+                            curr_max[0].max(x[0]),
+                            curr_max[1].max(x[1]),
+                            curr_max[2].max(x[2]),
+                        ]
+                    })
+                    .iter()
+                    .product::<usize>()
+            })
+            .sum::<usize>()
+            .into()
     }
 }
